4522fd8d13a8e6c7b9800860433657b6f41e6df6
nijat <ncthsynl@gmail.com>
2022-01-07 16:23:47 +0400

    configured env and added to gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { MongooseModule } from '@nestjs/mongoose'
 import { MongoProductModule } from './mongoProduct/mongoProduct.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ProductModule,
     MongooseModule.forRoot(
       'mongodb://localhost:27017/nestjs'
