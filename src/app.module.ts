e7f7e7c8295fb4dbdd32c625f99250dd929c57ac
nijat <ncthsynl@gmail.com>
2022-01-07 17:15:46 +0400

    created database.module and added app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { ProductModule } from './products/product.module';
 import { MongooseModule } from '@nestjs/mongoose'
 import { MongoProductModule } from './mongoProduct/mongoProduct.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASS: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      })
+    }),
+    DatabaseModule,
     ProductModule,
     MongooseModule.forRoot(
       'mongodb://localhost:27017/nestjs'
